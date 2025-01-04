642764584d1cc8da2cb94be20c2e06ca1c6bf649
Jakob Neumann <mail@polarfox.dev>
2025-01-04 19:09:31 +0100

    minor UI fixes and version number

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import packageJson from '../../package.json';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'strangui';
+  version = packageJson.version;
 }
