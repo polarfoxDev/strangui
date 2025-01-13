37738e6288073dbaf17c7a5df668d93ff5dc2b66
Jakob Neumann <mail@polarfox.dev>
2025-01-13 21:33:56 +0100

    fix loading riddles

--- src/app/core/strands.service.ts
+++ src/app/core/strands.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { GameState, Letter, RiddleConfig, Solution } from '../strands/models';
 import { AppStorage, SafeStorageAccessor } from './storage';
+import packageJson from '../../../package.json';
 
 @Injectable({
   providedIn: 'root'
@@ -29,7 +30,7 @@ export class StrandsService {
   }
 
   loadRiddle(date: string): Observable<RiddleConfig> {
-    return this.http.get<RiddleConfig>(`${this.BASE_URL}/riddle-${date}.json`);
+    return this.http.get<RiddleConfig>(`${this.BASE_URL}/riddle-${date}.json?appVersion=` + packageJson.version);
   }
 
   getGameStateAccessor(date: string, solutionFallback: Solution[], letterFallback: Letter[]): SafeStorageAccessor<GameState> {
