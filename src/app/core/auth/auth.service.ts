599d95ebf0f868281c52e138062627245887a0ba
Jakob Neumann <mail@polarfox.dev>
2025-06-07 12:26:21 +0200

    update to new token and registration system

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -4,11 +4,11 @@ import { Store } from '@ngrx/store';
 import { catchError, map, Observable } from 'rxjs';
 import { environment } from '@env/environment';
 import * as AuthAction from '@auth-state/auth.actions';
-import { SigninDto } from './dto/signin.dto';
 import { UserDto } from './dto/user.dto';
 
 interface AuthResponse {
   access_token: string;
+  refresh_token: string;
 }
 
 @Injectable({
@@ -18,16 +18,27 @@ export class AuthService {
   private http = inject(HttpClient);
   private store = inject(Store);
 
-  createAnonymousAccount(): Observable<SigninDto> {
-    return this.http.post<SigninDto>(new URL(`${environment.apiBaseUrl}/users`).toString(), {});
-  }
-
-  signIn(id: string, secret: string): Observable<void> {
-    return this.http.post<AuthResponse>(new URL(`${environment.apiBaseUrl}/auth/login`).toString(), { id, secret }).pipe(
+  signIn(userName: string, secret: string): Observable<void> {
+    return this.http.post<AuthResponse>(new URL(`${environment.apiBaseUrl}/auth/login`).toString(), { userName, secret }).pipe(
       map((response: AuthResponse) => {
         localStorage.setItem('auth_accessToken', response.access_token);
+        localStorage.setItem('auth_refreshToken', response.refresh_token);
       }),
     );
+  }
+
+  refreshToken(): Observable<void> {
+    const refreshToken = localStorage.getItem('auth_refreshToken');
+    return this.http.post<AuthResponse>(new URL(`${environment.apiBaseUrl}/auth/refresh`).toString(), { refreshToken }).pipe(
+      map((response: AuthResponse) => {
+        localStorage.setItem('auth_accessToken', response.access_token);
+        localStorage.setItem('auth_refreshToken', response.refresh_token);
+      }),
+    );
+  }
+
+  register(userName: string, secret: string): Observable<UserDto> {
+    return this.http.post<UserDto>(new URL(`${environment.apiBaseUrl}/users`).toString(), { userName, secret });
   }
 
   getUser(): Observable<UserDto> {
